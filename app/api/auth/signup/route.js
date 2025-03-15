e0afc78a0f53f2702e20163554dc815207263c67
Kongstrength <Sitthaphop.s@ku.th>
2025-03-15 11:53:33 +0700

    first commit

---
+++ app/api/auth/signup/route.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
+
+export async function POST(request) {
+  try {
+    const { name, email, password } = await request.json();
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    return Response.json({
+      message: "User created Successfully",
+      data: {
+        newUser
+      }
+    });
+  } catch (error) {
+    return Response.json({
+      error: error.message,
+    });
+  }
+}
